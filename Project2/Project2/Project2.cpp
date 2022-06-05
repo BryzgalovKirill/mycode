844bcf847b28a5d3847de3d59e3cd0f260560910
BryzgalovKirill <106826792+BryzgalovKirill@users.noreply.github.com>
2022-06-05 10:57:26 +0500

    Add files via upload

---
+++ Project2/Project2/Project2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    
+    cout << "Введите число: ";
+    int x;
+    cin >> x;
+
+    while (x < 10)
+    {
+        cout << x << " * " << x << " = " << x * x << endl;
+        x++;
+    }
+    
+    
+    return 0;
+}
